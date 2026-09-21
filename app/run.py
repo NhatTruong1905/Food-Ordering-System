461d315fd15251cfb619f15b1ef655ef079911bd
icondenimStore <tn696199@gmail.com>
2026-09-22 01:26:05 +0700

    deploy project

--- app/run.py
+++ app/run.py
@@ -8,6 +8,8 @@ if ROOT_DIR not in sys.path:
 from app import app, index, admin
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    debug = os.environ.get('FLASK_DEBUG', 'false').lower() in ['true', '1']
+    app.run(host='0.0.0.0', port=port, debug=debug)
 
 
